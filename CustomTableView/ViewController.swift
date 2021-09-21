810082e5e8838970a9bc43179a44c9c911996cb9
Xuanwei Zhang <christinezxw@hotmail.com>
2021-09-20 19:41:13 -0700

    add code

--- CustomTableView/ViewController.swift
+++ CustomTableView/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let arr = ["apple", "banana", "grape", "guava", "jujube", "lemon", "lychee", "peach", "strawberry", "watermelon"]
 
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.imgView.image = UIImage(named: arr[indexPath.row])
+        cell.lblImage.text = arr[indexPath.row]
+        return cell
+    }
 
 }
 
